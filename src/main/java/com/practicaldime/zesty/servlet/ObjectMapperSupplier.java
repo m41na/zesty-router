816d38ae694cc0f216078cd0c4093ad339011fcc
Stephen Maina <m41na@yahoo.cm>
2019-08-08 16:45:01 -0500

    changing data formating pattern for object mapper.

--- src/main/java/com/practicaldime/zesty/servlet/ObjectMapperSupplier.java
+++ src/main/java/com/practicaldime/zesty/servlet/ObjectMapperSupplier.java
@@ -1,16 +1,57 @@
 package com.practicaldime.zesty.servlet;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.function.Supplier;
 
 public class ObjectMapperSupplier implements Supplier<ObjectMapper> {
 
+    public static class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+        public LocalDateSerializer() {
+            super(LocalDate.class);
+        }
+
+        @Override
+        public void serialize(LocalDate value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+            generator.writeString(value.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        }
+    }
+
+    public static class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+
+        protected LocalDateDeserializer() {
+            super(LocalDate.class);
+        }
+
+        @Override
+        public LocalDate deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+            return LocalDate.parse(parser.readValueAs(String.class));
+        }
+    }
+
+    private final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .optionalStart()
+            .appendLiteral('T')
+            .appendOptional(DateTimeFormatter.ISO_TIME)
+            .toFormatter();
     private final ObjectMapper mapper = new ObjectMapper();
 
     public ObjectMapperSupplier() {
-        mapper.setDateFormat(new SimpleDateFormat("MMM dd, yyyy hh:mm:ss"));
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.sssZ"));
     }
 
     @Override
