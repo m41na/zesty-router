e3c7f407a815af6c085f577789bad7e51ae60788
Stephen Maina <m41na@yahoo.cm>
2019-09-22 00:14:35 -0500

    adding test module for zesty development.

---
+++ zesty-testing/src/test/java/com/practicaldime/testing/server/ZestyJUnit4ClassRunnerTest.java
@@ -0,0 +1,54 @@
+package com.practicaldime.testing.server;
+
+import com.practicaldime.zesty.app.AppServer;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import java.util.function.BiFunction;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(ZestyJUnit4ClassRunner.class)
+public class ZestyJUnit4ClassRunnerTest {
+
+    private BiFunction<Integer, Integer, Integer> addition = (x, y) -> x + y;
+
+    @ZestyProvider
+    public AppServer provider() {
+        AppServer server = new AppServer();
+        server.router().get("/hello", (req, res, done) -> done.resolve(CompletableFuture.runAsync(() -> {
+            res.send("hello from server");
+        })));
+        server.listen(8080, "localhost");
+        return server;
+    }
+
+    @Test
+    public void testAddition() {
+        System.out.println("executing testAddition");
+        assertEquals("addition", 8, addition.apply(5, 3).intValue());
+    }
+
+    @Test
+    public void testHelloFromServer(HttpClient client) throws InterruptedException, ExecutionException, TimeoutException {
+        ContentResponse response = client.newRequest("http://localhost:8080/hello")
+                .timeout(3, SECONDS)
+                .send();
+        assertEquals("Contains 'hello from server'", true, response.getContentAsString().contains("hello from server"));
+    }
+
+    @Test
+    public void testHelloFromServer2(HttpClient client) throws InterruptedException, ExecutionException, TimeoutException {
+        ContentResponse response = client.newRequest("http://localhost:8080/hello")
+                .timeout(3, SECONDS)
+                .send();
+        assertFalse("Contains 'hello from server++'", response.getContentAsString().contains("hello from server++"));
+    }
+}
---
+++ zesty-testing/src/test/java/com/practicaldime/testing/server/ZestySpringJUnit4ClassRunnerTest.java
@@ -0,0 +1,68 @@
+package com.practicaldime.testing.server;
+
+import com.practicaldime.zesty.app.AppServer;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import java.util.function.BiFunction;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(ZestySpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={ZestySpringJUnit4ClassRunnerTest.ZestyTestConfig.class})
+@TestPropertySource("/test-file.properties")
+public class ZestySpringJUnit4ClassRunnerTest {
+
+    @Configuration
+    public static class ZestyTestConfig{}
+
+    @Value("${x}")
+    private int x;
+    @Value("${y}")
+    private int y;
+
+    private BiFunction<Integer, Integer, Integer> addition = (x, y) -> x + y;
+
+    @ZestyProvider
+    public AppServer provider() {
+        AppServer server = new AppServer();
+        server.router().get("/hello", (req, res, done) -> done.resolve(CompletableFuture.runAsync(() -> {
+            res.send("hello from server");
+        })));
+        server.listen(8080, "localhost");
+        return server;
+    }
+
+    @Test
+    public void testAddition() {
+        System.out.println("executing testAddition");
+        assertEquals("addition", 300, addition.apply(x, y).intValue());
+    }
+
+    @Test
+    public void testHelloFromServer(HttpClient client) throws InterruptedException, ExecutionException, TimeoutException {
+        ContentResponse response = client.newRequest("http://localhost:8080/hello")
+                .timeout(3, SECONDS)
+                .send();
+        assertEquals("Contains 'hello from server'", true, response.getContentAsString().contains("hello from server"));
+    }
+
+    @Test
+    public void testHelloFromServer2(HttpClient client) throws InterruptedException, ExecutionException, TimeoutException {
+        ContentResponse response = client.newRequest("http://localhost:8080/hello")
+                .timeout(3, SECONDS)
+                .send();
+        assertFalse("Contains 'hello from server++'", response.getContentAsString().contains("hello from server++"));
+    }
+}
