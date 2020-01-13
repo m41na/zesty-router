762e54c8a21ea9c0cf04d57a001cac839381477c
Stephen Maina <m41na@yahoo.cm>
2020-01-13 07:17:39 -0600

    playing around with demos modules to see how best to work with AppLoader

--- zesty-demos/launch.sh
+++ zesty-demos/launch.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-java -cp .:/c/Works/Java/zesty-router/zesty-cli/target/zesty-cli-0.1.1.jar \
- com.practicaldime.zesty.cli.AppLoader
+JAVA_HOME="$GRAALVM_HOME"
+app_jar=zesty-demos-1.0-SNAPSHOT-shaded.jar
+rm -rf ./deploy
+mkdir -p ./deploy/bin
+cp ./target/"$app_jar" ./deploy
+cp ../zesty-cli/target/zesty-cli-0.1.1.jar ./deploy/bin
+cd ./deploy
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -cp ./bin/zesty-cli-0.1.1.jar com.practicaldime.zesty.cli.AppLoader
